55f1906444467218c3c69acbf2fef301223eabcb
Tiana Zhen <tianaz@uci.edu>
2025-11-04 17:18:55 -0800

    Init React + Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Hello, this is a test! 🚀
+      </h1>
+      <h1 className="text-6xl text-pink-500 underline">
+        Tailwind is working!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
